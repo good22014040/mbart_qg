1206241d45ae8b4f127d2835d64909c21caca29f
WEN-CHUAN HSU <82022181+good22014040@users.noreply.github.com>
2023-04-10 22:16:02 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
+train_data_num = -1
+eval_data_num = -1
 train_batch_size = 2
 eval_batch_size = 2
 num_train_epochs = 8
 generate_max_length = 128
 learning_rate = 3e-5
 gradient_accumulation_steps = 8
+best_model = "facebook/mbart-large-50"
